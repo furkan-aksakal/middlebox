a3fa91b40bace6e5407b515459995e008c3e0a76
furkan-aksakal <furkan.aksakal@metu.edu.tr>
2025-03-22 18:29:17 +0300

    Delay is added. NatsGateway IP chane for causing conflict.

--- code/python-processor/main.py
+++ code/python-processor/main.py
@@ -11,19 +11,16 @@ async def run():
 
     async def message_handler(msg):
         subject = msg.subject
-        data = msg.data #.decode()
-        #print(f"Received a message on '{subject}': {data}")
+        data = msg.data 
         packet = Ether(data)
         print(packet.show())
-        # Publish the received message to outpktsec and outpktinsec
-        #delay = random.expovariate(1 / 5e-6)
-        #await asyncio.sleep(delay)
+        delay = random.expovariate(1 / 5e-8)
+        await asyncio.sleep(delay)
         if subject == "inpktsec":
             await nc.publish("outpktinsec", msg.data)
         else:
             await nc.publish("outpktsec", msg.data)
    
-    # Subscribe to inpktsec and inpktinsec topics
     await nc.subscribe("inpktsec", cb=message_handler)
     await nc.subscribe("inpktinsec", cb=message_handler)
 
